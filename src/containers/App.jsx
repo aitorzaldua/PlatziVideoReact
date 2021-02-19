01bbed6bd345402d41809bb03bd272d12d186652
Aitor Zaldua <aitor.zaldua@gmail.com>
2021-02-19 12:29:12 +0100

    commit del 19 de febrero de 2021

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Header from '../components/Header';
 import Search from '../components/Search';
 import Categories from '../components/Categories';
@@ -7,42 +7,50 @@ import CarouselItem from '../components/CarouselItem';
 import Footer from '../components/Footer';
 import '../assets/styles/App.scss';
 
-const App = () => (
-  <div className='App'>
-    <Header />
-    <Search />
+const App = () => {
+  const [videos, setVideos] = useState({ mylist: [], trends: [], originals: [] });
 
-    <Categories title='Mi lista'>
-      <Carousel>
-        <CarouselItem />
-        <CarouselItem />
-        <CarouselItem />
-        <CarouselItem />
-      </Carousel>
-    </Categories>
+  useEffect(() => {
+    fetch('http://localhost:3000/initialState')
+      .then((response) => response.json())
+      .then((data) => setVideos(data));
 
-    <Categories title='Tendencias'>
-      <Carousel>
-        <CarouselItem />
-        <CarouselItem />
-        <CarouselItem />
-        <CarouselItem />
-      </Carousel>
-    </Categories>
+  }, []);
 
-    <Categories title='Originales'>
-      <Carousel>
-        <CarouselItem />
-        <CarouselItem />
-        <CarouselItem />
-        <CarouselItem />
-      </Carousel>
-    </Categories>
+  return (
+    <div className='App'>
+      <Header />
+      <Search />
 
-    <Footer />
+      {videos.mylist.length > 0 && (
+        <Categories title='Mi lista'>
+          <Carousel>
+            <CarouselItem />
+          </Carousel>
+        </Categories>
+      )}
 
-  </div>
-);
+      <Categories title='Tendencias'>
+        <Carousel>
+          {
+            // eslint-disable-next-line react/jsx-props-no-spreading
+            videos.trends.map((item) => <CarouselItem key={item.id} {...item} />)
+          }
+
+        </Carousel>
+      </Categories>
+
+      <Categories title='Originales'>
+        <Carousel>
+          <CarouselItem />
+        </Carousel>
+      </Categories>
+
+      <Footer />
+
+    </div>
+  );
+};
 
 export default App;
 
