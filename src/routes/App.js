d1cb2975dde91b31c0ef36c8e242cfb385a158bd
Aitor Zaldua <aitor.zaldua@gmail.com>
2021-05-17 17:14:34 +0200

    ultimo de hoy

--- src/routes/App.js
+++ src/routes/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 import Home from '../containers/Home';
+import Login from '../containers/Login';
 
 //Función para exponer a BrowserRouter
 
@@ -8,6 +9,7 @@ const App = () => (
 
   <BrowserRouter>
     <Route exact path='/' component={Home} />
+    <Route exact path='/login' component={Login} />
   </BrowserRouter>
 
 );
