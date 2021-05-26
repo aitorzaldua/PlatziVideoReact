08d64e7a0db5c6f3298264f1456e91e03f6a4570
Aitor Zaldua <aitor.zaldua@gmail.com>
2021-05-26 21:09:12 +0200

    commit by night

--- src/containers/Login.jsx
+++ src/containers/Login.jsx
@@ -4,6 +4,7 @@
 /* eslint-disable jsx-a11y/label-has-associated-control */
 
 import React from 'react';
+import { Link } from 'react-router-dom';
 import '../assets/styles/components/Login.scss';
 import googleIcon from '../assets/static/google-icon.png';
 import twitterIcon from '../assets/static/twitter-icon.png';
@@ -38,7 +39,9 @@ const Login = () => (
       </section>
       <p className='login__container--register'>
         No tienes ninguna cuenta
-        <a href=''>Regístrate</a>
+        <Link to='/register'>
+          Regístrate
+        </Link>
       </p>
     </section>
   </section>
